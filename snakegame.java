c32456950ad0b70b3bb0b28b9a5fd928ed070db8
rakeshkumarduggi7 <rakeshfc3@gmail.com>
2024-11-10 05:08:29 +0530

    body arry and snake food random places

--- snakegame.java
+++ snakegame.java
@@ -20,13 +20,9 @@ public class snakegame extends ApplicationAdapter {
 	float x, y;
 	BitmapFont bmf;
 	float angle;
-	float sbangle;
 	boolean start;
 	Texture snakebody;
-	float sbx;
-	float sby;
 	ArrayList<float[]> al;
-	ArrayList<TextureRegion> sba;
  ArrayList<float[]> bc;
 	ArrayList<int[]> foodco;
 	Random r=new Random();
@@ -40,18 +36,15 @@ public class snakegame extends ApplicationAdapter {
 		sb=new TextureRegion(snakebody);
 		x = 350;
 		y = 170;
-		sbx=318;
-		sby=170;
 		start =false;
 		bmf=new BitmapFont(Gdx.files.internal("fren.fnt"));
 		bmf.setColor(Color.GOLD);
 		angle=0;
-		sbangle=0;
-		sba=new ArrayList<>();
-		sba.add(sb);
+
+
 al=new ArrayList<>();
  bc=new ArrayList<>();
- bc.add(new float[]{318,170,0});
+ bc.add(new float[]{318,170,0,0});
 foodco=new ArrayList<>();
 		foodco.add(new int[]{r.nextInt(0,700),r.nextInt(0,330)});
 		foodco.add(new int[]{r.nextInt(0,700),r.nextInt(0,330)});
@@ -81,7 +74,8 @@ foodco=new ArrayList<>();
 					1f, 1f,                 // Scale
 					angle                     // Rotation angle in degrees
 			);
-		for (int i=0;i<bc.size();i++){
+		//body drawing---------------------------------------------------------------------------------
+		for(int i=0;i<bc.size();i++){
 		batch.draw(
 			 sb,
 				bc.get(i)[0], bc.get(i)[1],                   // Position of the image
@@ -96,108 +90,118 @@ foodco=new ArrayList<>();
 		batch.end();
 		if(Gdx.input.isKeyPressed(Input.Keys.ENTER))start=true;
 		if (start){
+			//head coordinates and moviement effect------------------------------------------------------
 		if(angle==0){
 			x+=Gdx.graphics.getDeltaTime()*200;
 			if (Gdx.input.isKeyPressed(Input.Keys.UP)){
 				angle=90;
-				al.add(new float[]{x,y,angle});
+				al.add( new float[]{x,y,angle});
 			}
 			else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)){
 				angle=270;
-				al.add(new float[]{x,y,angle});
+				al.add( new float[]{x,y,angle});
 			}
 		} else if (angle==90) {
 			y+=Gdx.graphics.getDeltaTime()*200;
-
 			if (Gdx.input.isKeyPressed(Input.Keys.LEFT)){
 				angle=180;
-				al.add(new float[]{x,y,angle});
+				al.add( new float[]{x,y,angle});
 			}
 			else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
 				angle=0;
-				al.add(new float[]{x,y,angle});
+				al.add( new float[]{x,y,angle});
 			}
 		}
 		else if (angle==180) {
 			x-=Gdx.graphics.getDeltaTime()*200;
 			if (Gdx.input.isKeyPressed(Input.Keys.UP)){
 				angle=90;
-				al.add(new float[]{x,y,angle});
+				al.add( new float[]{x,y,angle});
 			}
 			else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)){
 				angle=270;
-				al.add(new float[]{x,y,angle});
+				al.add( new float[]{x,y,angle});
 			}
 		}
 		else if (angle==270) {
 			y-=Gdx.graphics.getDeltaTime()*200;
 			if (Gdx.input.isKeyPressed(Input.Keys.LEFT)){
 				angle=180;
-				al.add(new float[]{x,y,angle});
+				al.add( new float[]{x,y,angle});
 			}
 			else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
 				angle=0;
-				al.add(new float[]{x,y,angle});
+				al.add( new float[]{x,y,angle});
 			}
 		}
-		for (float[] co:bc){
-			if(co[2]==0){
-				co[0]+=Gdx.graphics.getDeltaTime()*200;
-				if(al.size()!=0) {
-					if (al.get(0)[0]<=co[0]){
-						co[2]=al.get(0)[2];
-						al.remove(0);
+		//body-coordinates------------------------------------------------------------------------
+			for(int co=0;co<bc.size();co++){
+			if(bc.get(co)[2]==0){
+				bc.get(co)[0]+=Gdx.graphics.getDeltaTime()*200;
+				if(al.size()!=0&&bc.get(co)[3]<al.size()) {
+					if (al.get((int)bc.get(co)[3])[0]<=bc.get(co)[0]){
+						bc.get(co)[2]=al.get((int)bc.get(co)[3])[2];
+						bc.get(co)[3]=bc.get(co)[3]+1;
 					}
 				}
 			}
-			else if (co[2]==180) {
-				co[0]-=Gdx.graphics.getDeltaTime()*200;
-				if(al.size()!=0) {
-					if (al.get(0)[0]>=co[0]){
-						co[2]=al.get(0)[2];
-						al.remove(0);
+			else if (bc.get(co)[2]==180) {
+				bc.get(co)[0]-=Gdx.graphics.getDeltaTime()*200;
+				if(al.size()!=0&&bc.get(co)[3]<al.size()) {
+					if (al.get((int)bc.get(co)[3])[0]>=bc.get(co)[0]){
+						bc.get(co)[2]=al.get((int)bc.get(co)[3])[2];
+						bc.get(co)[3]=bc.get(co)[3]+1;
 					}
 				}
 			}
-			else if (co[2]==270) {
-				co[1]-=Gdx.graphics.getDeltaTime()*200;
-				if(al.size()!=0) {
-					if (al.get(0)[1]>=co[1]){
-						co[2]=al.get(0)[2];
-						al.remove(0);
+			else if (bc.get(co)[2]==270) {
+				bc.get(co)[1]-=Gdx.graphics.getDeltaTime()*200;
+				if(al.size()!=0&&bc.get(co)[3]<al.size()) {
+					if (al.get((int)bc.get(co)[3])[1]>=bc.get(co)[1]){
+						bc.get(co)[2]=al.get((int)bc.get(co)[3])[2];
+						bc.get(co)[3]=bc.get(co)[3]+1;
 					}
 				}
+			}
+			else if (bc.get(co)[2]==90) {
+				bc.get(co)[1]+=Gdx.graphics.getDeltaTime()*200;
+				if(al.size()!=0&&bc.get(co)[3]<al.size()) {
+					if (al.get((int)bc.get(co)[3])[1]<=bc.get(co)[1]){
+						bc.get(co)[2]=al.get((int)bc.get(co)[3])[2];
+						bc.get(co)[3]=bc.get(co)[3]+1;
+					}
+				}
+			}
+		}
 
-			}
-			else if (co[2]==90) {
-				co[1]+=Gdx.graphics.getDeltaTime()*200;
-				if(al.size()!=0) {
-					if (al.get(0)[1]<=co[1]){
-						co[2]=al.get(0)[2];
-						al.remove(0);
-					}
-				}
-			}
-		}}
+		}
 		if (x>=foodco.get(0)[0]&&x<=foodco.get(0)[0]+32 &&y>=foodco.get(0)[1]&&y<=foodco.get(0)[1]+32 ){
 			foodco.remove(0);
-
-		}
+if (bc.get(bc.size()-1)[2]==0){
+	bc.add( new float[]{bc.get(bc.size()-1)[0]-32,bc.get(bc.size()-1)[1],0,bc.get(bc.size()-1)[3]});
+} else  if (bc.get(bc.size()-1)[2]==90){
+	bc.add( new float[]{bc.get(bc.size()-1)[0],bc.get(bc.size()-1)[1]-32,90,bc.get(bc.size()-1)[3]});
+}else  if (bc.get(bc.size()-1)[2]==180){
+	bc.add( new float[]{bc.get(bc.size()-1)[0]+32,bc.get(bc.size()-1)[1],180,bc.get(bc.size()-1)[3]});
+}else  if (bc.get(bc.size()-1)[2]==270){
+	bc.add( new float[]{bc.get(bc.size()-1)[0],bc.get(bc.size()-1)[1]+32,270,bc.get(bc.size()-1)[3]});
+}
+}
 		if(x>=Gdx.graphics.getWidth()||x<0){
 			start=false;
-		al.clear();	sbx=318;
-			sby=170;
-			sbangle=0;
+		al.clear();
+			foodco.add(new int[]{r.nextInt(0,700),r.nextInt(0,330)});
+			foodco.add(new int[]{r.nextInt(0,700),r.nextInt(0,330)});
 			bc.clear();
-			bc.add(new float[]{318,170,0});
+			bc.add(new float[]{318,170,0,0});
 		}
 		if(y>=Gdx.graphics.getHeight()||y<0){
 			start=false;
-		al.clear();	sbx=318;
-			sby=170;
-			sbangle=0;
+		al.clear();
+			foodco.add(new int[]{r.nextInt(0,700),r.nextInt(0,330)});
+			foodco.add(new int[]{r.nextInt(0,700),r.nextInt(0,330)});
 			bc.clear();
-			bc.add(new float[]{318,170,0});
+			bc.add(new float[]{318,170,0,0});
 		}
 	}
 	@Override
